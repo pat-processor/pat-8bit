7425e95d4030a1f6d3c6a1eee05af4987d9231fb
Simon Hollis <simon@cs.bris.ac.uk>
2014-03-13 15:54:46 +0000

    Converted design to 20bits, closer to micro-code sytle. Fields are acessed in two cycles via negedge-triggered access.

--- patternbuf/pat.v
+++ patternbuf/pat.v
@@ -1,7 +1,7 @@
-module pat(pc, write_en, data_adr, data_out, bufp, fieldp, fieldwp, field_byte_out, instr, data_in, field_byte_in, clk) ;
+module pat(pc, write_en, data_adr, data_out, bufp, fieldp, fieldwp, field_out, imem_in, data_in, field_in, clk) ;
 
 parameter i_adr_width = 10 ; // instruction address space size
-parameter i_width = 16 ; // instruction width
+parameter i_width = 15 ; // instruction width
 parameter d_adr_width = 8 ; // data address space size
 parameter d_width = 8 ; // data width
 parameter call_stack_size = 8 ; // max call depth supported
@@ -9,10 +9,16 @@ parameter call_stack_pointer_size = 3 ; // bits for call stack pointer
 parameter bufp_width = 3 ;
 parameter fieldp_width = 5 ;
 parameter buffer_width = 8 ;
+parameter opcode_i8_width = 4 ; // width of opcode for i8 instruction
+parameter opcode_i3_width = 4 ; // width of opcode for i3 instruction
+parameter opcode_i0_width = 5 ; // width of opcode for i0 instruction
 
-input [i_width-1:0] instr ;
+`define i3_opcode_prefix 4'b1111  // prefix string from i8 space
+`define i0_opcode_prefix 4'b1111  // prefix string from i3 space
+
+input [i_width-1:0] imem_in ;
 input [d_width-1:0] data_in;
-input [buffer_width-1:0] field_byte_in ;
+input [buffer_width-1:0] field_in ;
 input clk ;
 
 output [i_adr_width-1:0] pc ;
@@ -22,7 +28,7 @@ output [d_width-1:0] data_out ;
 output [bufp_width-1:0] bufp ;
 output [fieldp_width-1:0] fieldp ;
 output [fieldp_width-1:0] fieldwp ;
-output [buffer_width-1:0] field_byte_out ;
+output [buffer_width-1:0] field_out ;
 
 reg [d_width-1:0] acc ; // the main accumulator
 reg [d_adr_width-1:0] sp ; // stack pointer
@@ -36,114 +42,84 @@ reg [d_width-1:0] data_out ;
 reg [bufp_width-1:0] bufp ;
 reg [fieldp_width-1:0] fieldp ;
 reg [fieldp_width-1:0] fieldwp ;
-reg [buffer_width-1:0] field_byte_out ;
+reg [buffer_width-1:0] field_out ;
+
+reg [d_width-1:0] field_value ; // after latching field in
+reg [d_width-1:0] dmem [256] ; // TODO: Select this memory or external
 
 
-
-/*
 // instruction type selection
-wire i_t_fb ;
-wire i_t_fi3 ;
+/*
+wire i_t_i0 ;
+wire i_t_i3 ;
 wire i_t_i8 ;
-wire i_t_ldif ;
 */
 
 // instruction immediate values
-wire [d_width-1:0] immediate ;
-wire [2:0] immediate3 ;
-wire [fieldp_width-1:0] field ;
-wire [bufp_width-1:0] buffer ;
-wire [i_width-1:0] opcode ;
+wire [7:0] immediate_i8 ;
+wire [2:0] immediate_i3 ;
+wire [fieldp_width-1:0] fieldp_next ;
+wire [i_width-1:0] instruction ;
 wire [1:0] condition ;
+wire [opcode_i8_width-1:0] opcode_i8 ;
+wire [opcode_i3_width-1:0] opcode_i3 ;
+wire [opcode_i0_width-1:0] opcode_i0 ;
 
-// individual format immediates
-wire [fieldp_width-1:0] field_ldif ;
-wire [1:0] condition_ldif ;
-wire [d_width-1:0] immediate_ldif ;
-assign {condition_ldif, field_ldif, immediate_ldif} = instr ;
+wire field_op ; // 0 := ACC op ; 1 := field op
 
-wire [(i_width-d_width)-3:0] opcode_i8 ;
-wire [1:0] condition_i8 ;
-wire [d_width-1:0] immediate_i8 ;
-assign {condition_i8, opcode_i8, immediate_i8} = instr ;
+assign {fieldp_next, condition, field_op, opcode_i8, immediate_i8} = imem_in ;
+//assign {fieldp_next, condition, field_op, opcode_i8, immediate_i8} = 20'b00001_11_1_1000_01010101 ;
 
-wire [(i_width-(fieldp_width+2+3+1)):0] opcode_fi3 ;
-wire [1:0] condition_fi3 ;
-wire [fieldp_width-1:0] field_fi3 ;
-wire [2:0] immediate3_fi3 ;
-assign {condition_fi3, opcode_fi3, field_fi3, immediate3_fi3} = instr ;
+assign opcode_i3 = imem_in[6:3] ; // TODO: parameterise
+assign immediate_i3 = imem_in[2:0] ; 
+assign opcode_i0 = imem_in[opcode_i0_width-1:0] ;
 
-// add opcode i3 and opcode i0
 
-task decode ;
-	input [i_width-1:0] instr ;
-	// outputs
-	
+task doOp ;
+	// check the opcode prefix to determine operation type.
 	begin
-	casex(instr)
-		// LDIF
-		{3'bxx1,{i_width-3{1'bx}}}: 
-		begin
-		        // call ldif instruction directly	
-			i_ldif(condition_ldif, field_ldif, immediate_ldif) ;
-		end
-
-		// i8 format instruction
-		{4'bxx01,{i_width-4{1'bx}}}:
-		begin
-			i_i8(opcode_i8, condition_i8, immediate_i8) ;
-		end
-
-		// fi3 format instruction
-		{5'bxx001,{i_width-5{1'bx}}}:
-		begin
-			i_fi3(opcode_fi3, condition_fi3, field_fi3, immediate3_fi3) ;
-		end
-
-		// i3 format instruction
-		{8'bxx000001,{i_width-8{1'bx}}}:
-		begin
-			i_i3(opcode_i3, condition_i3, immediate_i3) ;
-		end
-
-		{10'bxx00000001,{i_width-10{1'bx}}}:
-		begin
-			i_i0(opcode_i0, condition_i0) ;
-		end
-
-		default: $display ("Undefined instruction in decode") ;
-	endcase
-	end
-endtask // end decode task
-
-
-task i_ldif ;
-	input [1:0] condition ;
-	input [fieldp_width-1:0]field ;
-        input [d_width-1:0] immediate ;
-
-	begin
+		if (opcode_i8 != `i3_opcode_prefix) i_i8(opcode_i8, condition, immediate_i8) ;
+		else if (opcode_i3 != `i0_opcode_prefix) i_i3(opcode_i3, condition, immediate_i3) ;
+		else i_i0(opcode_i0, condition) ;
 	end
 endtask
+
+task incPC ; // incrementPC if not a jump
+	begin
+		pc <= pc + 1 ;
+	end
+endtask
+
+
 
 task i_i8 ;
-	input [i_width-1:0] opcode ;
+	input [opcode_i8_width-1:0] opcode ;
 	input [1:0] condition ;
-        input [d_width-1:0] immediate ;
+        input [7:0] immediate ;
+	casex(opcode) // case is evaluated in order
 
-	begin
-	end
+		// i8 format instruction
+		4'b0000: op_bf(immediate) ;
+		4'b0001: op_bb(immediate) ;
+		4'b0010: op_call(immediate) ;
+		4'b0011: op_ldi(immediate) ;
+		4'b0100: op_ldm(immediate) ;
+		4'b0101: op_stm(immediate) ;
+		4'b0110: op_setsp(immediate) ;
+		// 4'b0111:
+		4'b1000: op_or(immediate) ;
+		4'b1001: op_and(immediate) ;
+		4'b1010: op_addm(immediate) ;
+		4'b1011: op_subm(immediate) ;
+		4'b1100: op_add(immediate) ;
+		4'b1101: op_sub(immediate) ;
+		// 4'b1110:
+
+
+		default: $display ("Undefined i8 instruction in decode") ;
+	endcase
 endtask
 
-task i_fi3 ;
-	input [i_width-1:0] opcode ;
-	input [1:0] condition ;
-	input [fieldp_width-1:0] field ;
-        input [2:0] immediate3 ;
-
-	begin
-	end
-endtask
 
 task i_i3 ;
 	input [i_width-1:0] opcode ;
@@ -164,15 +140,14 @@ task i_i0 ;
 endtask
 
 
-task bf
+task op_bf ;
 	input [7:0] offset ;
-
 	begin
 		pc <= pc + offset ;
 	end
 endtask
 
-task bb
+task op_bb ;
 	input [7:0] offset ;
 
 	begin
@@ -180,7 +155,7 @@ task bb
 	end
 endtask
 
-task call
+task op_call ;
 	input [7:0] offset ;
 	begin
 		call_stack[call_stack_pointer] <= pc ;
@@ -189,110 +164,210 @@ task call
 	end
 endtask
 
-task return
+
+task op_return ; // 'return' is a reserved keyword
 	begin
 		pc <= call_stack[call_stack_pointer - 1] ;
 		call_stack_pointer <= call_stack_pointer - 1 ;
 	end
 endtask
 
-task ldi
+
+task op_ldi ;
 	input [7:0] immediate ;
-	begin
-		acc <= immediate ;
-	end
+	fork
+		if (field_op)
+			field_out <= immediate ;
+		else
+			acc <= immediate ;
+		incPC() ;
+	join
 endtask
 
-task setsp
+task op_setsp ;
 	input [7:0] immediate ;
-	begin
+	fork
 		sp <= immediate ;
-	end
+		incPC() ;
+	join
 endtask
 
-task incsp
+task op_incsp ;
 	input [2:0] inc ;
-	begin
+	fork
 		sp <= sp + inc ;
-	end
+		incPC() ;
+	join
 endtask
 
-task decsp
+task op_decsp ;
 	input [2:0] dec ;
 	begin
 		sp <= sp - dec ;
+		incPC() ;
 	end
 endtask
 
-task ora
+task op_or ;
 	input [7:0] immediate ;
-	begin
-		acc <= acc | immediate ;
-	end
+	fork
+		if (field_op)
+			field_out <= field_value | immediate ;
+		else
+			acc <= acc | immediate ;
+		incPC() ;
+	join
 endtask
 
-task anda
+task op_and ;
 	input [7:0] immediate ;
-	begin
-		acc <= acc & immediate ;
-	end
+	fork
+		if (field_op)
+			field_out <= field_value & immediate ;
+		else
+			acc <= acc & immediate ;
+		incPC() ;
+	join
 endtask
 
-task adda
+task op_add ;
 	input [7:0] immediate ;
-	begin
-		acc <= acc + immediate ;
-	end
+	fork
+		if (field_op)
+			field_out <= field_value + immediate ;
+		else
+			acc <= acc + immediate ;
+		incPC() ;
+	join
 endtask
 
-task suba
+task op_sub ;
 	input [7:0] immediate ;
-	begin
-		acc <= acc - immediate ;
-	end
+	fork
+		if (field_op)
+			field_out <= field_value - immediate ;
+		else
+			acc <= acc - immediate ;
+		incPC() ;
+	join
 endtask
 
-task shla
+task op_shl ;
 	input [2:0] shift ;
-	begin
-		acc <= acc << shift ;
-	end
+	fork
+		if (field_op)
+			field_out <= field_value << shift ;
+		else
+			acc <= acc << shift ;
+		incPC() ;
+	join
 endtask
 
-task shra
+task op_shr ;
 	input [2:0] shift ;
-	begin
-		acc <= acc >> shift ;
-	end
+	fork
+		if (field_op)
+			field_out <= field_value >> shift ;
+		else
+			acc <= acc >> shift ;
+		incPC() ;
+	join
 endtask
 
-task ashra
+task op_ashr ;
 	input [2:0] shift ;
+	fork
+		if (field_op)
+			field_out <= field_value >>> shift ;
+		else
+			acc <= acc >>> shift ;
+		incPC() ;
+	join
+endtask
+
+task op_not ;
+	fork
+		if (field_op)
+			field_out <= ~field_value ;
+		else
+			acc <= ~acc ;
+		incPC() ;
+	join
+endtask
+
+task op_ldm ;
+	input [7:0] immediate ;
+	fork
+		if (field_op)
+			field_out <= dmem[immediate] ;
+		else
+			acc <= dmem[immediate] ;
+		incPC() ;
+	join
+endtask 
+
+task op_stm ;
+	input [7:0] immediate ;
+	fork
+		if (field_op)
+			dmem[immediate] <= field_value ;
+		else
+			dmem[immediate] <= acc ;
+		incPC() ;
+	join
+endtask 
+
+
+task op_addm ;
+	input [7:0] immediate ;
+	fork
+		if (field_op)
+			field_out <= field_value + dmem[immediate] ;
+		else
+			acc <= acc + dmem[immediate] ;
+		incPC() ;
+	join
+endtask 
+
+task op_subm ;
+	input [7:0] immediate ;
+	fork
+		if (field_op)
+			field_out <= field_value - dmem[immediate] ;
+		else
+			acc <= acc - dmem[immediate] ;
+		incPC() ;
+	join
+endtask 
+
+
+// control tasks
+
+task getField() ;
 	begin
-		acc <= acc >>> shift ;
+		field_value <= field_in ;
 	end
 endtask
 
-task nota
+task updateFieldp() ;
 	begin
-		acc <= ~acc ;
+		fieldp <= fieldp_next ;
 	end
 endtask
-
-
-task ldm
-	input [7:0] address ;
-	begin
-//		acc <= 
-		// how to access memory? From the immediate??
 
 
 always @(posedge clk)
-	fork // run next statements in parallel
-		// pc needs updating in each instruction
-		pc <= pc + 1 ;
-		decode(instr) ;
+	fork
+		doOp() ;
+		// updatePC() ; // part of the ops
+		// commitResults() ; // part of the ops
 	join
 
+always @(negedge clk)
+	fork // run next statements in parallel
+		getField() ;
+		updateFieldp() ;
+		// getDmem() ; // if external data memory used
+	join
 
 endmodule
