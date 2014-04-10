de98ef5ed9a321282872e67eef54eb89f47f4068
Simon 'Harry' Hollis <simon@cs.bris.ac.uk>
2014-04-11 00:39:46 +0100

    Implement patasm output to file.

--- patasm/patasm.py
+++ patasm/patasm.py
@@ -3,6 +3,11 @@
 from __future__ import print_function
 import sys
 
+input_file_name = 'pat.asm'
+input_file = open(input_file_name, 'r')
+
+output_file_name = 'pat.hex'
+
 # i8 operators
 I8_OPS = ["ORI", "ANDI", "ADDM", "SUBM", "ADDI", "SUBI", "LDI", "LDM", "BF", "BB", "ORM", "ANDM"] 
 I8_OPCODES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xd, 0xe]
@@ -19,8 +24,6 @@ CONDITIONS = ["Z", "LT", "GT", "AL"]
 COND_CODES = [0, 1, 2, 3]
 
 
-input_file_name = 'pat.asm'
-input_file = open(input_file_name, 'r')
 
 def get_condition(instr): 
 	cond = 3 # default to ALWAYS
@@ -91,6 +94,26 @@ def encode_instr(fieldp, cond, opcode, shift, dest, immediate):
 		return ((fieldp << 15) | (cond << 13) | (dest << 12) | opcode)
 
 
+def write_hexfile(mem):
+	out = open(output_file_name, 'w')
+	address = 0
+	while (address < len(mem)):
+		out.write('@'+str(address/2)+' ')
+		if (address + 1 < len(mem)):
+			first = mem[address]
+			second = mem[address+1]
+			print(first+second)
+			out.write(first+second)
+		else:
+			first = mem[address]
+			second = "00000"
+			print(first+second)
+			out.write(first+second)
+		out.write("\n")
+		address += 2	
+	out.close()
+
+##################################
 address = 0
 mem = []
 labels = {}
@@ -130,14 +153,15 @@ for instr in input_file:
 		dest = get_dest(instr)
 		#print (cond, opcode, shift, dest, immediate, instr)
 		emit = encode_instr(0, cond, opcode, shift, dest, immediate)
-		emit = (hex(emit)[2:]) # to hex and lose the '0x' prefix
+		emit = (hex(emit)[2:]).zfill(5) # to hex, pad with zeroes and lose the '0x' prefix
 		#print(emit)
 		mem.append(emit) 
 		address += 1
-	
-address = 0
-for item in mem:
-	print(address, item)
-	address += 1
+
+write_hexfile(mem)	
+#address = 0
+#for item in mem:
+#	print(address, item)
+#	address += 1
 
 
