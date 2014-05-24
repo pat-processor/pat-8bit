50c97819e73a22b1d5ab3fbe8b8a1728688bbf6a
Simon Hollis <simon@cs.bris.ac.uk>
2014-05-24 14:25:40 +0100

    Complete analogue pin removal.

--- patternbuf/synthesized2/pads.tcl
+++ patternbuf/synthesized2/pads.tcl
@@ -103,7 +103,7 @@ editPin -side Right -fixedPin 1 -unit TRACK -fixOverlap 1 -layer 3 -spreadType s
 
 editPin -side Right -fixedPin 1 -unit TRACK -fixOverlap 1 -layer 3 -spreadType start -spacing 4.0 -start 1390.163 1610.59 -pin {{bufp_low[0]} {bufp_low[1]} {bufp_low[2]} {field_fromPAT_low[0]} {field_fromPAT_low[1]} {field_fromPAT_low[2]} {field_fromPAT_low[3]} {field_fromPAT_low[4]} {field_fromPAT_low[5]} {field_fromPAT_low[6]} {field_fromPAT_low[7]} {field_toPAT_low[0]} {field_toPAT_low[1]} {field_toPAT_low[2]} {field_toPAT_low[3]} {field_toPAT_low[4]} {field_toPAT_low[5]} {field_toPAT_low[6]} {field_toPAT_low[7]} field_write_en_low {fieldp_low[0]} {fieldp_low[1]} {fieldp_low[2]} {fieldp_low[3]} {fieldp_low[4]} {fieldwp_low[0]} {fieldwp_low[1]} {fieldwp_low[2]} {fieldwp_low[3]} {fieldwp_low[4]} {saddr_low[0]} {saddr_low[1]} {saddr_low[2]} sclk_low sin_low sout_low ssel_low}
 
-editPin -side Right -fixedPin 1 -unit TRACK -fixOverlap 1 -layer 3 -spreadType start -spacing 4.0 -start 1499.353 1210.848 -pin {clock_external clock_select f5v_select vref_select}
+editPin -side Right -fixedPin 1 -unit TRACK -fixOverlap 1 -layer 3 -spreadType start -spacing 4.0 -start 1499.353 1210.848 -pin {clock_select f5v_select vref_select}
 
 editPin -side Right -fixedPin 1 -unit TRACK -fixOverlap 1 -layer 3 -spreadType start -start 1495.065 1345.472 -pin clk_int
 
