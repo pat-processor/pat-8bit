26d3a6c38acc7c7859e54df62ece9d64651da999
Simon Hollis <simon@cs.bris.ac.uk>
2014-07-30 15:20:44 +0100

    Clean up.

--- patternbuf/pat.sv
+++ patternbuf/pat.sv
@@ -144,12 +144,11 @@ assign op_return = (opcode_i0 == 4'b0011) && i_t_i0 ;
 assign op_nop = (opcode_i0 == 4'b0101) && i_t_i0 ;
 
 // operation type selection
-wire source_field, source_imm, source2_imm, source_in ;
+wire source_field, source_imm, source_in ;
 wire dest_reg, dest_field, dest_pc, dest_out, dest_null ;
 
 assign source_field = field_op ;
 assign source_imm = (i_t_i8 && (opcode_i8[0] == 0)) | (i_t_i3 && (opcode_i3[0] == 0)) ;
-assign source2_imm = !(op_shlzr | op_shlor | op_shrzr | op_asrr) ;
 assign source_in = op_in ;
 
 
