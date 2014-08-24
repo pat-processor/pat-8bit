dd07929746320bf9b2da4615e40fdbdcdc8cd995
Simon Hollis <simon@cs.bris.ac.uk>
2014-08-24 17:26:42 +0100

    Parameterise signing script.

--- patternbuf/synthesized2/sign.tcl
+++ patternbuf/synthesized2/sign.tcl
@@ -1,54 +1,80 @@
 # Sign the design!
+set scalefactor 5
+set xoffset 0
+set yoffset 0
+
+
+proc scaledpoly {{inx 0} {iny 0}} {
+	global scalefactor
+	global xoffset
+	global yoffset
+
+	set newx [expr $inx * $scalefactor]
+	set nexx [expr $newx + [expr $xoffset * $scalefactor] ]
+	set newy [expr $iny * $scalefactor]
+	set nexy [expr $newy + [expr $xoffset * $scalefactor] ]
+
+	editAddPoly $newx $newy
+}
+
+proc scaledpolycommit {{inx 0} {iny 0}} {
+	global scalefactor
+	global xoffset
+	global yoffset
+
+	set newx [expr $inx * $scalefactor]
+	set nexx [expr $newx + [expr $xoffset * $scalefactor] ]
+	set newy [expr $iny * $scalefactor]
+	set nexy [expr $newy + [expr $xoffset * $scalefactor] ]
+
+	editCommitPoly $newx $newy
+}
+
+
 setEdit -layer_polygon AM
 # S
-editAddPoly 0 50
-editAddPoly 30 50
-editAddPoly 30 40
-editAddPoly 10 40
-editAddPoly 10 30
-editAddPoly 30 30
-editAddPoly 30 0
-editAddPoly 0 0
-editAddPoly 0 10
-editAddPoly 20 10
-editAddPoly 20 20
-editCommitPoly 0 20
 
-deselectAll
-selectWire 0.0000 0.0000 30.0000 50.0000 6 gnd!
-editMove 35 75
+
+scaledpoly 0 50
+scaledpoly 30 50
+scaledpoly 30 40
+scaledpoly 10 40
+scaledpoly 10 30
+scaledpoly 30 30
+scaledpoly 30 0
+scaledpoly 0 0
+scaledpoly 0 10
+scaledpoly 20 10
+scaledpoly 20 20
+scaledpolycommit 0 20
+
 
 # J
-editAddPoly 0 0
-editAddPoly 0 15
-editAddPoly 15 15
-editAddPoly 15 35
-editAddPoly 0 35
-editAddPoly 0 50
-editAddPoly 35 50
-editAddPoly 30 35
-editAddPoly 25 35
-editCommitPoly 25 0
+scaledpoly 0 0
+scaledpoly 0 15
+scaledpoly 15 15
+scaledpoly 15 35
+scaledpoly 0 35
+scaledpoly 0 50
+scaledpoly 35 50
+scaledpoly 30 35
+scaledpoly 25 35
+scaledpolycommit 25 0
 
-deselectAll
-selectWire 0.0000 0.0000 35.0000 50.0000 6 gnd!
-editMove 85 75
 
 
 # H
-editAddPoly 0 0
-editAddPoly 0 50
-editAddPoly 10 50
-editAddPoly 10 30
-editAddPoly 20 30
-editAddPoly 20 50
-editAddPoly 30 50
-editAddPoly 30 0
-editAddPoly 20 0
-editAddPoly 20 20
-editAddPoly 10 20
-editCommitPoly 10 0
+scaledpoly 0 0
+scaledpoly 0 50
+scaledpoly 10 50
+scaledpoly 10 30
+scaledpoly 20 30
+scaledpoly 20 50
+scaledpoly 30 50
+scaledpoly 30 0
+scaledpoly 20 0
+scaledpoly 20 20
+scaledpoly 10 20
+scaledpolycommit 10 0
 
-deselectAll
-selectWire 0.0000 0.0000 30.0000 50.0000 6 gnd!
-editMove 135 75
+
